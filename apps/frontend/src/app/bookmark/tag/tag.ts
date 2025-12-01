6268c479bbe3638f12e4d89b97abd72045641b94
Moamen Hraden <moamen.hraden@primion.eu>
2025-12-02 00:51:10 +0100

    fix: remove unused imports

--- apps/frontend/src/app/bookmark/tag/tag.ts
+++ apps/frontend/src/app/bookmark/tag/tag.ts
@@ -1,34 +1,9 @@
-import { Component,  inject, input } from '@angular/core';
-import { BookmarkService } from '../bookmark.service';
-import { MatCard, MatCardTitle, MatCardHeader, MatCardActions, MatCardSubtitle } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
-import { httpResource, HttpResourceRef } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
-import { TitleCasePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   templateUrl: './tag.html',
-  imports: [
-    MatCard,
-    MatCardActions,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatIcon,
-    TitleCasePipe
-  ],
 })
 export class Tag  {
-  
-  #bookmarkService = inject(BookmarkService)
 
   id = input.required<string>()
-/* 
-  collection!: HttpResourceRef<ICollection | undefined>
-  bookmarks!: HttpResourceRef<IGetResponse<IBookmark[]> | undefined>
-
-  constructor() {
-    this.collection = httpResource<ICollection>(() => `${environment.apiUrl}/collections/tags/records/${this.id()}`,)
-    this.bookmarks = httpResource<IGetResponse<IBookmark[]>>(() => `${environment.apiUrl}/collections/bookmarks/records?filter=(tags~'${this.id()}')&expand=tags`,)
-  } */
 }
